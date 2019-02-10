9d03e4a5686b232b2c4c59d3471588e3c5f455a6
wongband <bryanwong7788@gmail.com>
2019-02-10 14:40:09 -0800

    Refactored fade in and out feature

--- Assets/Js/IHEOP.js
+++ Assets/Js/IHEOP.js
@@ -1,17 +1,10 @@
 const sub = document.getElementsByClassName('colored_box');
 window.addEventListener('scroll', () => {
-  console.log(sub[1].offsetTop);
-  console.log(window.scrollY);
-  console.log(sub[0].offsetTop);
-  if((window.scrollY > (sub[1].offsetTop/2)) && (window.scrollY < sub[1].offsetTop)) {
-      sub[1].style.opacity = '1.0';
-  } else {
-      sub[1].style.opacity = '0.0';
-  }
-
-  if((window.scrollY > (sub[0].offsetTop/2)) && (window.scrollY < sub[0].offsetTop)) {
-      sub[0].style.opacity = '1.0';
-  } else {
-      sub[0].style.opacity = '0.0';
+  for (let i = 0; i < sub.length; i++) {
+    if((window.scrollY > (sub[i].offsetTop/2)) && (window.scrollY < sub[i].offsetTop)) {
+        sub[i].style.opacity = '1.0';
+    } else {
+        sub[i].style.opacity = '0.0';
+    }
   }
 });
