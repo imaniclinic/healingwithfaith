484b509b11a1b776a0f84f927cdb341140dcf4b0
wongband <bryanwong7788@gmail.com>
2019-02-27 21:04:23 -0800

    Added the toggle hamburger option

--- Assets/Js/Fade.js
+++ Assets/Js/Fade.js
@@ -1,10 +1,31 @@
-const sub = document.getElementsByClassName('colored_box');
-window.addEventListener('scroll', () => {
-  for (let i = 0; i < sub.length; i++) {
-    if((window.scrollY > (sub[i].offsetTop/1.5)) && (window.scrollY < sub[i].offsetTop)) {
-        sub[i].style.opacity = '1.0';
-    } else {
-        sub[i].style.opacity = '0.0';
+window.onload = () => {
+  toggle();
+  scroll();
+}
+
+const scroll = () => {
+  const sub = document.getElementsByClassName('colored_box');
+  window.addEventListener('scroll', () => {
+    for (let i = 0; i < sub.length; i++) {
+      if((window.scrollY > (sub[i].offsetTop/1.5)) && (window.scrollY < sub[i].offsetTop)) {
+          sub[i].style.opacity = '1.0';
+      } else {
+          sub[i].style.opacity = '0.0';
+      }
     }
-  }
-});
+  });
+}
+// navbar-dropdown probally changes the display. So set it to none or whatever it is called.
+const toggle = () => {
+  let burger = document.getElementsByClassName("navbar-burger")[0];
+  let menu = document.getElementsByClassName("navbar-menu")[0];
+
+  burger.addEventListener("click", () => {
+    if (menu.className === "navbar-menu is-active")
+    {
+      menu.className = "navbar-menu";
+    } else {
+      menu.className = "navbar-menu is-active";
+    }
+  });
+}
