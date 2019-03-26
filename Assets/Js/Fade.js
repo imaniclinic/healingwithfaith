a02f9ab1a2fb828616604b654bc789d94b597d70
wongband <bryanwong7788@gmail.com>
2019-03-26 13:45:40 -0700

    Added board members and honary members image to SCC

--- Assets/Js/Fade.js
+++ Assets/Js/Fade.js
@@ -8,7 +8,7 @@ const scroll = () => {
   const sub = document.getElementsByClassName('colored_box');
   window.addEventListener('scroll', () => {
     for (let i = 0; i < sub.length; i++) {
-      if((window.scrollY > (sub[i].offsetTop/1.5)) && (window.scrollY < sub[i].offsetTop)) {
+      if((window.scrollY > (sub[i].offsetTop/2)) && (window.scrollY < sub[i].offsetTop)) {
           sub[i].style.opacity = '1.0';
       } else {
           sub[i].style.opacity = '0.0';
