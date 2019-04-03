e5920f57bd5fc50ac92b25d915d398a3d5e07d5d
RishuvDutta <RishuvDutta@github.com>
2019-04-02 18:48:43 -0700

    start of final imani website

---
+++ FinalImani/scripts/contact.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#submit').click(function() {
+        // code here will send email 
+    });
+}); // email form
+
+$(window).resize(function() {
+    if ($(window).width() < 480) {
+        
+    } else {
+
+    }
+}); // changes on resize
