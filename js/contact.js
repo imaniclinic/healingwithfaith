d6506833b98f77e7d1ac0ca61f46650391fe450e
Nicholas Steven <nicholassteven998@gmail.com>
2019-04-08 19:33:47 -0700

    Commit issue fix

--- js/contact.js
+++ js/contact.js
@@ -2,12 +2,15 @@ $(document).ready(function() {
     $('#submit').click(function() {
         // code here will send email 
     });
+
+    /*
+    $(window).resize(function() {
+        if ($(this).width() > 800) {
+            $('#bg').css('background-image', "url('index.jpg')");
+        } else { 
+            // default setting for desktop here...
+            $('#bg').css('background-image', 'none');
+        }
+    });*/
 }); // email form
 
-$(window).resize(function() {
-    if ($(window).width() < 480) {
-        
-    } else {
-
-    }
-}); // changes on resize
